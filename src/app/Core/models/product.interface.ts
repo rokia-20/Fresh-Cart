48de353db3b230e58b21dea378f2eec5aab8d369
rokia-20 <rokarefaar88@gmail.com>
2026-02-21 06:31:11 +0200

    Remove Subcategory interface
    Removed Subcategory interface from product.interface.ts

--- src/app/Core/models/product.interface.ts
+++ src/app/Core/models/product.interface.ts
@@ -18,12 +18,7 @@ export interface Product {
     id: string;
 }
 
-export interface Subcategory {
-    _id: string;
-    name: string;
-    slug: string;
-    category: string;
-}
+
 
 export interface Category {
     _id: string;
